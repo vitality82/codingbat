56b9f66d0346495a9b35458dad8229bcd362ca7b
vitality82 <petarr@gmail.com>
2014-12-28 23:42:20 +0100

    Update sumDigits.java

--- recursion-1/sumDigits.java
+++ recursion-1/sumDigits.java
@@ -1,13 +1,13 @@
-Task:
+/*
 Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12). 
-
-sumDigits(126) › 9
-sumDigits(49) › 13
-sumDigits(12) › 3
-
-Solution:
+sumDigits(126) â€º 9
+sumDigits(49) â€º 13
+sumDigits(12) â€º 3
+*/
 
 public int sumDigits(int n) { 
-  if (n == 0) return 0;
+  if (n == 0) 
+    return 0;
+    
   return n % 10 + sumDigits(n / 10);
 }

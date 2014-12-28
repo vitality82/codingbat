a19c58cca022f75c4fd0628893840fbbc06a1cb1
vitality82 <petarr@gmail.com>
2014-12-28 23:42:44 +0100

    Update triangle.java

--- recursion-1/triangle.java
+++ recursion-1/triangle.java
@@ -1,16 +1,16 @@
-Task:
-
+/*
 We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, 
 the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows. 
 
-triangle(0) › 0
-triangle(1) › 1
-triangle(2) › 3
-
-Solution:
+triangle(0) â€º 0
+triangle(1) â€º 1
+triangle(2) â€º 3
+*/
 
 public int triangle(int rows) {
- if(rows == 0) return 0;
+ if(rows == 0) 
+  return 0;
+  
  return triangle(rows-1)+rows;
 }
 

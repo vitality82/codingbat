ac232a4f8571e1c058bf7e5af8be564de9119b54
vitality82 <petarr@gmail.com>
2014-12-28 23:40:18 +0100

    Update factorial.java

--- recursion-1/factorial.java
+++ recursion-1/factorial.java
@@ -1,14 +1,15 @@
-TASK:
+/*
 Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. 
 Compute the result recursively (without loops). 
 factorial(1) › 1
 factorial(2) › 2
 factorial(3) › 6
+*/
 
-SOLUTION:
 public int factorial(int n) {
-  if(n<=0) return 0;
-  if(n==1) return 1;
+  if(n==1) 
+    return 1;
+    
   return n*factorial(n-1);
 }
 

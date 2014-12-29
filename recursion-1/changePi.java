4a07ce4523cf5d3ca38fd8d8560234ba95fcfc73
Petar <petarr@gmail.com>
2014-12-29 20:27:29 +0100

    commit

--- recursion-1/changePi.java
+++ recursion-1/changePi.java
@@ -7,12 +7,12 @@ changePi("pip") › "3.14p"
 */
 
 public String changePi(String str) {
-  
   if(str.length() < 2) 
-    return "";
+    return str;
     
   if(str.charAt(0)=='p' && str.charAt(1)=='i')
-    return "3.14"+ changePi(str.substring(2)); 
+    return "3.14"+ changePi(str.substring(2));
+   
   else
     return Character.toString(str.charAt(0))+changePi(str.substring(1));
 }
--- recursion-1/endX.java
+++ recursion-1/endX.java
@@ -6,13 +6,12 @@ endX("xxhixx") › "hixxxx"
 endX("xhixhix") › "hihixxx"
 */
 
-public String endX(String str) {
-  
+public String endX(String str) {  
   if(str.length() <=1 )
     return str;
     
   if(str.charAt(0) == 'x')
-    return endX(str.substring(1)) + 'x';
-  
-  return str.charAt(0) + endX(str.substring(1));
+    return endX(str.substring(1)) + str.charAt(0);
+  else 
+    return str.charAt(0) + endX(str.substring(1));
 }
